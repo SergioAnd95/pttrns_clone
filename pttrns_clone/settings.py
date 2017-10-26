9abf6161f5f285f17aef94368dcc78f5a2f79295
SergioAnd95 <developinc@yadnex.ru>
2017-10-26 20:17:30 +0300

    turn DEBUG to True in settings.py

--- pttrns_clone/settings.py
+++ pttrns_clone/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0d+(2%hyv3lkx7_--%%&5z=$75bk4^pkdy-n0+)uhft$ai)@jk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
